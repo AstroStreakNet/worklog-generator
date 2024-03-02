8c4a12710dec3495a1ec44013d2c5a2e6a81d5e7
karsh <utkarsh@cyberfear.com>
2024-03-02 16:11:59 +1100

    better format input
    instead of using dictionaries for storing user input, now im using
    arrays as that could be directly passed to the table generation function
    provided by the pdf generator library.
    
    additionally, i've implemented my own text wrapper to break long
    sentences into smaller ones for the length required. by no means is this
    perfect, infact, my approach only considers monospace font.
    
    Could perhaps use some external library to improve this functionality,
    however for the time being this approach isn't half bad.

--- main.py
+++ main.py
@@ -2,41 +2,59 @@
 
 import write 
 
-name = ""
-studentID = 1234
+name = "Utkarsh Ranjan"
+studentID = 102874485
+
+
+def wrap_input(prompt, limit):
+    while len(prompt) < 11:
+        prompt += ' '
+
+    input_text = input(f"\033[32m{prompt}: \033[0m")
+    wrapped_text = ""
+
+    while len(input_text) > limit:
+        space_index = input_text.rfind(' ', 0, limit)
+        
+        if space_index == -1:  
+            space_index = limit
+
+        wrapped_text += input_text[:space_index] + "\n"
+        input_text = input_text[space_index + 1:]
+
+    wrapped_text += input_text
+    return wrapped_text
 
 def main():
-    current_entries = []
-    next_entries = []
+    current_tasks = [] 
+    next_tasks = []
 
     week = input("\033[36mWeek Number: \033[0m")
 
     t_this_week = int(input("\n\033[35mNumber of Tasks: \033[0m"))
     for i in range(t_this_week):
-        entry = {
-                "task"  : input(f"\033[32mTask {i+1} \033[0m: "),
-                "status": input("\033[32mStatus \033[0m: "),
-                "time"  : input("\033[32mTime   \033[0m: "),
-                "note"  : input("\033[32mNote   \033[0m: ")
-                }
+        entry = []
+        entry.append(wrap_input(f"Task {i+1}", 52))
+        entry.append(wrap_input("Status", 15))
+        entry.append(wrap_input("Time hh:mm", 10))
+        entry.append(wrap_input("Note", 28))
         print("-")
 
-        current_entries.append(entry)
+        current_tasks.append(entry)
 
 
     t_next_week = int(input("\n\033[35mNumber of tasks for next week: \033[0m"))
     for i in range(t_next_week):
-        entry = {
-                "task"  : input(f"\033[31mTask {i+1} \033[0m: "),
-                "time"  : input("\033[31mTime   \033[0m: "),
-                }
+        entry = []
+        entry.append(wrap_input(f"Task {i+1}", 90))
+        entry.append(wrap_input("Time", 25))
         print("-")
 
-        next_entries.append(entry)
+        next_tasks.append(entry)
 
     summary = input("\033[35mSummary/Reflection:\033[0m\n")
 
-    write.generate(name, studentID, week, current_entries, next_entries, summary)
+    write.generate(name, studentID, week, current_tasks, next_tasks, summary)
 
 if __name__ == "__main__":
     main()
